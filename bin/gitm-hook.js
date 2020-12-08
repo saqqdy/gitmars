797ff78fc4a2730bfb8bff7561fad5ead514e350
saqqdy <saqqdy@qq.com>
2020-12-08 19:47:00 +0800

    gitm hook & gitm log

--- bin/gitm-hook.js
+++ bin/gitm-hook.js
@@ -9,17 +9,27 @@ const { gitDir } = require('./js/global')
 const ora = require('ora')
 /**
  * gitm hook
+ * gitm hook init
+ * gitm hook test
+ * gitm hook remove
+ * gitm hook config
  */
 program
 	.name('gitm hook')
-	.usage('[version]')
-	.description('升级gitmars')
-	.arguments('[version]')
+	.usage('[command]')
+	.description('git hook钩子')
+	.arguments('[command]')
 	.option('--no-verify', '是否需要跳过校验权限', false)
 	.option('-s, --since [since]', '查询在某个时间之后的日志，填写格式：10s/2m/2h/3d/4M/5y', '7d')
 	.option('-l, --limit [limit]', '做多查询的日志条数', 20)
 	.option('-b, --branches [branches]', '要查询的分支')
-	.action(async (version, opt) => {
+	.action(async (command, opt) => {
+		/**
+		 * 1. 是否合并过dev
+		 * 2. 1周内是否同步过上游分支代码
+		 * 3. 主干分支推送的内容是否是merge内容，暂时只检测最后一条记录
+		 * 4.
+		 */
 		const current = getCurrent()
 		const mainLogs = getLogs({
 			since: opt.since,
