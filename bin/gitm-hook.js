84656cf94adc3256523c2ae7a3ad8e89a54ced8d
saqqdy <saqqdy@qq.com>
2020-12-11 00:14:00 +0800

    gitmars hooks

--- bin/gitm-hook.js
+++ bin/gitm-hook.js
@@ -21,52 +21,81 @@ program
 	.arguments('[command]')
 	.option('--no-verify', '是否需要跳过校验权限', false)
 	.option('-s, --since [since]', '查询在某个时间之后的日志，填写格式：10s/2m/2h/3d/4M/5y', '7d')
-	.option('-l, --limit [limit]', '做多查询的日志条数', 20)
+	.option('-l, --limit [limit]', '最多查询的日志条数')
 	.option('-b, --branches [branches]', '要查询的分支')
 	.action(async (command, opt) => {
 		/**
-		 * 1. 是否合并过dev
+		 * 1. 是否合并过dev post-merge
 		 * 2. 1周内是否同步过上游分支代码
 		 * 3. 主干分支推送的内容是否是merge内容，暂时只检测最后一条记录
 		 * 4.
 		 */
 		const current = getCurrent()
-		const mainLogs = getLogs({
-			since: opt.since,
-			limit: opt.limit,
-			branches: opt.branches
-		})
-		const currentLogs = getLogs({
-			since: opt.since,
-			limit: opt.limit,
-			branches: 'wu'
-		})
-		const meagedDev = msg = sh.exec('git branch --contains feature/wu', { silent: true }).stdout
-		let isUpdated = false,
-			mainVers = [],
-			currentVers = [],
-			lastCommit = {
-				isMerge: false,
-				msg: '',
-				author: 'saqqdy',
-				date: ''
-			}
-		mainLogs.forEach(log => {
-			mainVers.push(log['%H'])
-		})
-		currentLogs.forEach(log => {
-			let arr = log['%P'] ? log['%P'].split(' ') : []
-			arr.forEach(item => {
-				currentVers.push(item)
-			})
-		})
-		mainVer: for (let ver of mainVers) {
-			if (currentVers.includes(ver)) {
-				isUpdated = true
-				break mainVer
-			}
+		let lastCommit = {
+			isMerge: false,
+			msg: '',
+			author: 'saqqdy',
+			date: ''
 		}
-		console.log(current, mainLogs, currentLogs, mainVers, currentVers)
+		// 1. 获取是否合并过dev
+		const getIsMergedBranch = (branch = 'dev') => {
+			const result = sh.exec(`git branch --contains ${current}`, { silent: true }).stdout.replace(/[\n\s]*$/g, '')
+			return result.split('\n').includes(branch)
+		}
+		// 2. 获取一周内是否同步过上游分支代码
+		const getIsUpdatedInTime = () => {
+			let isUpdated = false,
+				mainVers = [],
+				currentVers = []
+			const mainLogs = getLogs({
+				since: opt.since,
+				limit: opt.limit,
+				branches: opt.branches
+			})
+			const currentLogs = getLogs({
+				since: opt.since,
+				limit: opt.limit,
+				branches: current
+			})
+			mainLogs.forEach(log => {
+				mainVers.push(log['%H'])
+			})
+			currentLogs.forEach(log => {
+				let arr = log['%P'] ? log['%P'].split(' ') : []
+				arr.forEach(item => {
+					currentVers.push(item)
+				})
+			})
+			mainVer: for (let ver of mainVers) {
+				if (currentVers.includes(ver)) {
+					isUpdated = true
+					break mainVer
+				}
+			}
+			// console.log(current, mainLogs, currentLogs)
+			console.log(mainVers, currentVers)
+			return isUpdated
+		}
+		// 3. 获取主干分支推送的内容是否是merge内容，暂时只检测最后一条记录
+		const getIsMergeAction = () => {
+			const currentLogs = getLogs({
+				limit: 1,
+				branches: current
+			})
+			let p = currentLogs[0]['%P'] ? currentLogs[0]['%P'].split(' ') : []
+			// console.log(currentLogs)
+			return p.length > 1
+		}
+		// 4. 获取当前本地分支与远程分支的差别
+		const getIsNeedPull = () => {
+			sh.exec(`git fetch`, { silent: true })
+			const result = sh.exec(`git log ${current}..origin/${current}`, { silent: true }).stdout.replace(/[\n\s]*$/g, '')
+			return !!result
+		}
+		console.log('1. 当前分支是否合并过dev', getIsMergedBranch())
+		console.log('2. 一周内是否同步过上游分支代码', getIsUpdatedInTime())
+		console.log('3. 最后一条记录是否merge记录', getIsMergeAction())
+		console.log('4. 是否需要pull代码', getIsNeedPull())
 		// console.log('gitm hook working!', config, global, gitDir)
 		// console.log(getStatusInfo(), 99)
 		// let allow = [config.master],
