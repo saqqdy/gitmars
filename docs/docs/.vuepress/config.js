d7c18e6905d5cf9836d9878fbaf8313182911d0c
saqqdy <saqqdy@qq.com>
2021-02-19 20:05:56 +0800

    docs

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const settings = require('./settings.json')
+
 module.exports = {
     title: 'gitmars',
     description: '移动端UI组件库',
@@ -87,29 +89,25 @@ module.exports = {
     plugins: [
         // 官方回到顶部插件
         '@vuepress/back-to-top',
-
         //官方图片放大组件 目前是所有img都可以点击放大。具体配置见https://v1.vuepress.vuejs.org/zh/plugin/official/plugin-medium-zoom.html
         ['@vuepress/medium-zoom', { selector: 'img' }],
-
         // vssue 一个借助issue的评论插件 具体配置见https://vssue.js.org/zh/
         [
             '@vssue/vuepress-plugin-vssue',
             {
                 // 设置 `platform` 而不是 `api` 我这里是在github平台
                 platform: 'github',
-
                 // owner与repo配置 https://github.com/${owner}/${repo}
                 // 例如我的仓库地址为https://github.com/saqqdy/gitmars 则owner为saqqdy，repo为gitmars
-                owner: 'your owner',
-                repo: 'your repo',
-
+                owner: 'saqqdy',
+                repo: 'gitmars',
                 // 填写自己的OAuth App 信息。详见https://vssue.js.org/zh/options/#repo
-                clientId: 'clientId',
-                clientSecret: 'clientSecret',
+                clientId: settings.clientId,
+                clientSecret: settings.clientSecret,
                 locale: 'zh', //使用的语言  这里是简体中文
-                baseURL: 'https://github.com'
+                baseURL: 'https://github.com' //平台的 base URL
             }
-        ] //平台的 base URL
+        ]
     ],
     markdown: {
         lineNumbers: true,
