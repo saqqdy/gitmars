67cd95352a00fbd637bbb64f1a300552a99df3b1
saqqdy <saqqdy@qq.com>
2021-07-04 14:19:08 +0800

    update支持--all

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,8 @@
-import VueHighlightJS from 'vue-highlight.js'
+// import VueHighlightJS from 'vue-highlight.js'
 
 // 这里样式我选择的是atom-one-light；样式更多选择可以参见 https://highlightjs.org/static/demo/ 里的styles
 // 注意： 代码块的背景色 还是由官方设置的 $codeBgColor 决定的
-import 'highlight.js/styles/atom-one-light.css'
+// import 'highlight.js/styles/atom-one-light.css'
 
 // 你可以在这里引入自己定义的css样式，进行修改
 // import '../.vuepress/public/css/index.css'
@@ -16,5 +16,5 @@ export default ({
     isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
     // ...做一些其他的应用级别的优化
-    Vue.use(VueHighlightJS)
+    // Vue.use(VueHighlightJS)
 }
