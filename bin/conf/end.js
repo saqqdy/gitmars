9e153b1fd99fa546968843ab360a3587a7530717
saqqdy <saqqdy@qq.com>
2021-08-29 00:34:38 +0800

    配置导出方式优化

--- bin/conf/end.js
+++ bin/conf/end.js
@@ -13,5 +13,8 @@
     if (typeof exports === 'object' && typeof module === 'object') module.exports = cmdConfig
     // else if (typeof define === 'function' && define.amd) define(['cmdConfig'], () => cmdConfig)
     else if (typeof exports === 'object') exports['cmdConfig'] = cmdConfig
-    else root['cmdConfig'] = cmdConfig
+    else {
+        if (!root.gitmarsCmdConfig) root.gitmarsCmdConfig = {}
+        root.gitmarsCmdConfig['end'] = cmdConfig
+    }
 })(typeof window !== 'undefined' ? window : global)
--- bin/conf/update.js
+++ bin/conf/update.js
@@ -51,5 +51,8 @@
     if (typeof exports === 'object' && typeof module === 'object') module.exports = cmdConfig
     // else if (typeof define === 'function' && define.amd) define(['cmdConfig'], () => cmdConfig)
     else if (typeof exports === 'object') exports['cmdConfig'] = cmdConfig
-    else root['cmdConfig'] = cmdConfig
+    else {
+        if (!root.gitmarsCmdConfig) root.gitmarsCmdConfig = {}
+        root.gitmarsCmdConfig['update'] = cmdConfig
+    }
 })(typeof window !== 'undefined' ? window : global)
