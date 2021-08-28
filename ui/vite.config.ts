02b515c002742d8dec434ede25d1c4dbde13f997
saqqdy <saqqdy@qq.com>
2021-08-29 00:58:13 +0800

    ui项目迁移到vite-ts框架

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -38,46 +38,37 @@ export default defineConfig({
 		preprocessorOptions: {
 			less: {
 				modifyVars: {
-					green: '#17c0ae',
-					blue: '#409eff',
-					orange: '#fe703f',
-					red: ' #ef4f4f',
-					black: 'rgb(48, 49, 51)',
-					gray: '#909399',
-					success: ' #67c23a',
-					warning: ' #e6a23c',
-					error: ' #ef4f4f',
-					fontColor: '#5a5a5a',
-					borderColor: '#ebeef5'
-					// 'button-primary-background-color': 'red',
-					// 'primary-color': '#1890ff', // 全局主色
-					// 'link-color': '#fff', // 链接色
-					// 'success-color': '#67c23a', // 成功色
-					// 'warning-color': '#e6a23c', // 警告色
-					// 'error-color': ' #ef4f4f', // 错误色
-					// 'font-size': '20px', // 主字号
-					// blue: '#396eff',
-					// 'light-blue': '#a1e7ff',
-					// yellow: '#ffb22e',
-					// green: '#95ff00',
-					// cyan: '#00ffe6 ',
-					// 'light-cyan': '#3fffe6',
-					// rose: '#ff1fa3',
-					// 'light-rose': '#ff6788',
-					// 'heading-color': 'rgba(255, 255, 255, 0.85)', // 标题色
-					// 'text-color': 'rgba(255, 255, 255, 0.5)', // 主文本色
-					// 'text-color-secondary': 'rgba(255, 255, 255, 0.45)', // 次文本色
-					// 'disabled-color': 'rgba(255, 255, 255, 0.25)', // 失效色
-					// 'border-radius': '4px', // 组件/浮层圆角
-					// 'border-color': '#ebeef5', // 边框色
-					// 'box-shadow': '0 2px 8px rgba(255, 255, 255, 0.15)', // 浮层阴影}
+					// green: '#17c0ae',
+					// blue: '#409eff',
+					// orange: '#fe703f',
+					// red: ' #ef4f4f',
+					// black: 'rgb(48, 49, 51)',
+					// gray: '#909399',
+					// success: ' #67c23a',
+					// warning: ' #e6a23c',
+					// error: ' #ef4f4f',
+					// fontColor: '#5a5a5a',
+					// borderColor: '#ebeef5',
+					'primary-color': '#409eff', // 全局主色
+					'link-color': '#fff', // 链接色
+					'success-color': '#67c23a', // 成功色
+					'warning-color': '#e6a23c', // 警告色
+					'error-color': ' #ef4f4f', // 错误色
+					'font-size-base': '14px', // 主字号
+					'heading-color': 'rgba(255, 255, 255, 0.85)', // 标题色
+					'text-color': 'rgba(255, 255, 255, 0.65)', // 主文本色
+					'text-color-secondary': 'rgba(255, 255, 255, 0.45)', // 次文本色
+					'disabled-color': 'rgba(255, 255, 255, 0.25)', // 失效色
+					'border-radius-base': '4px', // 组件/浮层圆角
+					'border-color-base': '#ebeef5', // 边框色
+					'box-shadow-base': '0 2px 8px rgba(255, 255, 255, 0.15)' // 浮层阴影
 					// hack: `true; @import "assets/css/var.less";`,
 				},
 				javascriptEnabled: true
 			},
-			scss: {
-				// additionalData: '@import "src/assets/styles/vars.scss";',
-			}
+			// scss: {
+			// 	additionalData: '@import "src/assets/styles/vars.scss";',
+			// }
 		}
 	},
 	server: {
@@ -89,25 +80,5 @@ export default defineConfig({
 			}
 		},
 		port: 8080
-		// headers: {
-		// 	'Access-Control-Allow-Origin': '*',
-		// },
 	}
-	// css: {
-	//     // requireModuleExtension: true,
-	//     loaderOptions: {
-	//         css: {
-	//             importLoaders: 2,
-	//             import: true,
-	//             modules: {
-	//                 // localIdentName: '[path][name]__[local]--[hash:base64:5]',
-	//                 localIdentName: '[path][local]_[hash:base64:5]',
-	//             },
-	//             localsConvention: 'camelCaseOnly',
-	//         },
-	//         // postcss: {
-	//         //     plugins: [new aspectRatio(), new pxtovw(postcss)],
-	//         // },
-	//     },
-	// },
 })
