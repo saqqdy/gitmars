d52dd692b006a4c67b43f2f654ad018b771a3cad
saqqdy <saqqdy@qq.com>
2021-09-12 19:37:55 +0800

    修复upgrade的一个问题

--- bin/gitm-upgrade.ts
+++ bin/gitm-upgrade.ts
@@ -2,8 +2,12 @@
 const { program } = require('commander')
 const { spawnSync } = require('child_process')
 const sh = require('shelljs')
+const { options, args } = require('./conf/upgrade')
 const { success } = require('./js/index')
+const { createArgs } = require('./js/tools')
 const ora = require('ora')
+
+import { GitmarsOptionOptionsType } from '../typings'
 
 interface GitmBuildOption {
     mirror: boolean
@@ -12,58 +16,33 @@ interface GitmBuildOption {
 /**
  * gitm upgrade
  */
-program
-    .name('gitm upgrade')
-    .usage('[version]')
-    .description('升级gitmars')
-    .arguments('[version]')
-    .option('-m, --mirror', '是否使用淘宝镜像', false)
-    .action(async (version: string, opt: GitmBuildOption) => {
-        const spinner = ora(success('正在安装请稍后')).start()
-        // let match = (version && version.match(/[0-9.]+$/)) || null,
-        // 	cmd = `install -g gitmars@${match ? match[0] : 'latest'} ${opt.mirror ? '--registry=https://registry.npm.taobao.org' : ''}`,
-        // 	install = await execa('npm', cmd.split(' '), { cwd: process.cwd() }),
-        // 	ver = await execa('gitm', ['-v'], { cwd: process.cwd() })
-        // spinner.stop()
-        // sh.echo(install.stdout + `\n${success('安装完成')}`)
-        // sh.echo(ver.stdout)
-
-        const match = (version && version.match(/[0-9.]+$/)) || null
-        const cmdAdd: any[] = ['npm', ['install', '-g', `gitmars@${match ? match[0] : 'latest'}`]]
-        const cmdDel = ['npm', ['uninstall', '-g', 'gitmars']]
-        if (opt.mirror) cmdAdd[1] = cmdAdd[1].concat(['-registry', 'https://registry.npm.taobao.org'])
-        const uninstall = spawnSync(cmdDel[0], cmdDel[1], { stdio: 'inherit', shell: process.platform === 'win32' /*, env: { detached: true }*/ })
-        if (uninstall.status === 0) {
-            console.warn('安装出错了')
-            process.exit(0)
-        }
-        const install = spawnSync(cmdAdd[0], cmdAdd[1], { stdio: 'inherit', shell: process.platform === 'win32' /*, env: { detached: true }*/ })
-        spawnSync('gitm', ['-v'], { stdio: 'inherit', shell: process.platform === 'win32' /*, env: { detached: true }*/ })
-        sh.echo(`\n${success('安装完成')}`)
-        spinner.stop()
-        if (install.status === 0) {
-            process.exit(0)
-        } else {
-            console.warn('安装出错了')
-            process.exit(0)
-        }
-
-        // let match = (version && version.match(/[0-9.]+$/)) || null,
-        // 	cmd = [
-        // 		{
-        // 			cmd: `npm install -g gitmars@${match ? match[0] : 'latest'} ${opt.mirror ? '--registry=https://registry.npm.taobao.org' : ''}`,
-        // 			config: { again: true, kill: false, success: '升级成功', fail: '升级失败，请重试' }
-        // 		},
-        // 		{
-        // 			cmd: `gitm -v`,
-        // 			config: { again: false }
-        // 		}
-        // 	]
-        // queue(cmd).then(data => {
-        // 	if (data[0].code === 0) {
-        // 		sh.echo(data[1].out)
-        // 	}
-        // })
-    })
+program.name('gitm upgrade').usage('[version]').description('升级gitmars')
+if (args.length > 0) program.arguments(createArgs(args))
+options.forEach((o: GitmarsOptionOptionsType) => {
+    program.option(o.flags, o.description, o.defaultValue)
+})
+// .option('-m, --mirror', '是否使用淘宝镜像', false)
+program.action(async (version: string, opt: GitmBuildOption) => {
+    const spinner = ora(success('正在安装请稍后')).start()
+    const match = (version && version.match(/[0-9.]+$/)) || null
+    const cmdAdd: any[] = ['npm', ['install', '-g', `gitmars@${match ? match[0] : 'latest'}`]]
+    const cmdDel = ['npm', ['uninstall', '-g', 'gitmars']]
+    if (opt.mirror) cmdAdd[1] = cmdAdd[1].concat(['-registry', 'https://registry.npm.taobao.org'])
+    const uninstall = spawnSync(cmdDel[0], cmdDel[1], { stdio: 'inherit', shell: process.platform === 'win32' /*, env: { detached: true }*/ })
+    if (uninstall.status !== 0) {
+        console.warn('卸载出错了')
+        process.exit(0)
+    }
+    const install = spawnSync(cmdAdd[0], cmdAdd[1], { stdio: 'inherit', shell: process.platform === 'win32' /*, env: { detached: true }*/ })
+    spawnSync('gitm', ['-v'], { stdio: 'inherit', shell: process.platform === 'win32' /*, env: { detached: true }*/ })
+    sh.echo(`\n${success('安装完成')}`)
+    spinner.stop()
+    if (install.status === 0) {
+        process.exit(0)
+    } else {
+        console.warn('安装出错了')
+        process.exit(0)
+    }
+})
 program.parse(process.argv)
 export {}
