fa241c2ec213d197d96631ebf34ca7b08fe0ed92
saqqdy <saqqdy_wu@kingdee.com>
2021-09-13 14:09:05 +0800

    remove server dir

--- server/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const path = require('path');
-const cwd = process.cwd();
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-// console.log(__dirname, process.cwd())
-const adapter = new FileSync(path.join(__dirname, 'db.json'));
-const db = low(adapter);
-module.exports = db;
