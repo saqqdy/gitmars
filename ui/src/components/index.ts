03dce26f4c76197baa2b44828246675bc5546009
saqqdy <saqqdy@qq.com>
2021-10-17 00:06:32 +0800

    remove checkbox componant

--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,6 +1,5 @@
 import v3Split from './split/index.vue'
 import v3Button from './button/index.vue'
-import v3Checkbox from './checkbox/index.vue'
 import v3Collapse from './collapse/index.vue'
 import v3CollapseItem from './collapse-item/index.vue'
 import v3Box from './box/index.vue'
@@ -8,7 +7,6 @@ import v3Box from './box/index.vue'
 const install = app => {
 	app.component(v3Split.name, v3Split)
 	app.component(v3Button.name, v3Button)
-	app.component(v3Checkbox.name, v3Checkbox)
 	app.component(v3Collapse.name, v3Collapse)
 	app.component(v3CollapseItem.name, v3CollapseItem)
 	app.component(v3Box.name, v3Box)
@@ -18,7 +16,6 @@ export default {
 	install,
 	v3Split,
 	v3Button,
-	v3Checkbox,
 	v3Collapse,
 	v3CollapseItem
 }
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { ElProgress, ElSkeleton, ElRow, ElCol, ElButton, ElCarousel, ElForm, ElInput, ElSelect, ElTooltip, ElMessage, ElMessageBox } from 'element-plus'
+import { ElProgress, ElSkeleton, ElCheckbox, ElRow, ElCol, ElButton, ElCarousel, ElForm, ElInput, ElSelect, ElTooltip, ElMessage, ElMessageBox } from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 // @ts-ignore
 import plugins from '@/plugins'
@@ -16,6 +16,7 @@ app.config.globalProperties.$ELEMENT = { size: 'small', zIndex: 5000 }
 
 app.use(ElProgress)
 app.use(ElSkeleton)
+app.use(ElCheckbox)
 app.use(ElRow)
 app.use(ElCol)
 app.use(ElButton)
