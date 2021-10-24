c6b1ff82c96a172464bb313f6c35a00ce6745237
saqqdy <saqqdy@qq.com>
2021-10-24 16:02:14 +0800

    docs & ui --port

---
+++ app/www/assets/use-current-instance-legacy.88ced7af.js
@@ -0,0 +1,1 @@
+System.register(["./vendor-legacy.97ede141.js"],(function(e){"use strict";var t,r;return{setters:[function(e){t=e.al,r=e.S}],execute:function(){e("u",(function(e){"string"==typeof e&&(e=[e]);var n=r(),o=n.appContext,s=n.proxy,i=o.config.globalProperties,u={};return e&&e.includes("style")&&(u=t()),{globalProperties:i,proxy:s,style:u}}))}}}));
