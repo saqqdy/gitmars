f0c02393891227303e92587c466b34e48f998119
saqqdy <saqqdy@qq.com>
2021-11-05 16:34:03 +0800

    combine和end指令判断是否合并过dev默认使用远程分支

--- bin/gitm-combine.ts
+++ bin/gitm-combine.ts
@@ -181,7 +181,7 @@ program.action(
                 // 判断是否同步过dev分支
                 if (
                     !opt.dev &&
-                    !getIsMergedTargetBranch(`${type}/${name}`, config.develop)
+                    !getIsMergedTargetBranch(`${type}/${name}`, config.develop, true)
                 ) {
                     sh.echo(
                         warning(
