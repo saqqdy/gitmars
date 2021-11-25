6741a8ceac80d660f8b5eef9707beea77b8489d6
saqqdy <saqqdy@qq.com>
2021-11-26 00:05:26 +0800

    重构代码

--- typedoc.js
+++ typedoc.js
@@ -1,7 +1,17 @@
 module.exports = {
-    out: 'docs',
-    entryPoints: ['src/index.ts'],
-    json: 'docs/out.json',
+    out: 'typedoc',
+    entryPoints: [
+        'bin/core/api/index.ts',
+        'bin/core/build/index.ts',
+        'bin/core/cache/index.ts',
+        'bin/core/git/index.ts',
+        'bin/core/go/index.ts',
+        // 'bin/core/hook/index.ts',
+        'bin/core/shell/index.ts',
+        'bin/core/utils/index.ts',
+        'bin/core/index.ts'
+    ],
+    json: 'typedoc/out.json',
     name: 'index.md',
     includeVersion: true,
     readme: 'README.md'
