fbf5fce6a8738a575b772bfe5fea134be7fa5d4a
saqqdy <saqqdy@qq.com>
2021-12-15 14:57:20 +0800

    git status - unmerged files

--- bin/core/git/checkGitStatus.ts
+++ bin/core/git/checkGitStatus.ts
@@ -11,7 +11,12 @@ const { debug } = require('../utils/debug')
 function checkGitStatus(): boolean {
     const sum = getGitStatus({ stdio: 'inherit' })
     debug('checkGitStatus', sum)
-    if (sum.A.length > 0 || sum.D.length > 0 || sum.M.length > 0) {
+    if (
+        sum.A.length > 0 ||
+        sum.D.length > 0 ||
+        sum.M.length > 0 ||
+        sum.UU.length > 0
+    ) {
         sh.echo(
             red('您还有未提交的文件，请处理后再继续') +
                 '\n如果需要暂存文件请执行: gitm save\n恢复时执行：gitm get'
