3a9850291d4d8629386a59c8aef47dbc8c1d980e
saqqdy <saqqdy@qq.com>
2022-05-05 00:42:09 +0800

    format code

--- packages/server/src/helper/static-html.js
+++ packages/server/src/helper/static-html.js
@@ -2,11 +2,11 @@ const express = require('express')
 const notFind = require('./404')
 
 module.exports = function (path) {
-	return [
-		express.static(path, {
-			maxAge: 0,
-			immutable: true
-		}),
-		notFind
-	]
+    return [
+        express.static(path, {
+            maxAge: 0,
+            immutable: true
+        }),
+        notFind
+    ]
 }
