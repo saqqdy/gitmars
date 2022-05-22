22e3e064999e972d5d3dffbef1adae269e6c0d58
saqqdy <saqqdy@qq.com>
2022-05-22 23:16:24 +0800

    install command

--- packages/gitmars/src/conf/install.ts
+++ packages/gitmars/src/conf/install.ts
@@ -9,9 +9,64 @@ import type { GitmarsOptionType } from '../../typings'
                 name: 'pluginName',
                 variadic: false,
                 description: '插件名称'
+            },
+            {
+                required: false,
+                name: 'version',
+                variadic: false,
+                validator: (val, opts, cb) => {
+                    if (/\s+/.test(val)) {
+                        cb(new Error('请不要输入空格'))
+                        return
+                    }
+                    cb()
+                },
+                // transformer: null,
+                description: '版本号'
             }
         ],
-        options: [],
+        options: [
+            {
+                flags: '-m, --mirror',
+                required: false,
+                optional: false,
+                variadic: false,
+                mandatory: false,
+                short: '-m',
+                long: '--mirror',
+                negate: false,
+                description: '是否使用淘宝镜像',
+                defaultValue: false
+            },
+            {
+                flags: '-c, --client [client]',
+                required: false,
+                optional: true,
+                variadic: false,
+                mandatory: false,
+                short: '-c',
+                long: '--client',
+                negate: false,
+                description: '用于装包的客户端名称',
+                defaultValue: 'npm',
+                recommend: true,
+                value: 'npm'
+            },
+            {
+                flags: '-r, --registry <registry>',
+                required: true,
+                optional: true,
+                variadic: false,
+                mandatory: false,
+                short: '-r',
+                long: '--registry',
+                negate: false,
+                description: '使用镜像地址',
+                defaultValue: '',
+                recommend: true,
+                value: 'https://registry.npmmirror.com'
+            }
+        ],
         // 校验传值
         validatorOpts: (val, opts, cb) => {
             cb()
--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
+console.log('@gitmars/ui', __dirname, require.resolve('@gitmars/server'))
+// node require.resolve('@gitmars/server')
 require('@gitmars/server')
