981c228a8b0d692ff9ec76b7904042467de81027
saqqdy <saqqdy@qq.com>
2022-05-22 23:58:51 +0800

    install command

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,9 +1,12 @@
+const { join } = require('path')
 const express = require('express')
 const router = express.Router()
 
 const { index } = require('../controller')
+const [, uiBin] = process.argv
+const distPath = join(uiBin, '../../lib/node_modules/@gitmars/ui/dist')
 
-console.log(100, __dirname, process.argv)
-router.use('/', index())
+console.log(100, __dirname, process.argv, uiBin, distPath)
+router.use('/', index(distPath))
 
 module.exports = router
