cb38469a51445d6cda458dd5196f3cdbe010389b
saqqdy <saqqdy@qq.com>
2022-05-23 21:14:08 +0800

    3.0.0-beta.1

--- packages/server/src/routes/cmd.ts
+++ packages/server/src/routes/cmd.ts
@@ -1,7 +1,6 @@
 import type { NextFunction, Request, Response } from 'express'
 const express = require('express')
 const router = express.Router()
-// const fs = require('fs')
 const {
     enterDir,
     getBranchList,
@@ -9,17 +8,6 @@ const {
     getStatus,
     readFile
 } = require('../controller/cmd')
-// const sh = require('shelljs')
-// const glob = require('@gitmars/core/lib/global')
-// const getCurrentBranch = require('@gitmars/core/lib/git/getCurrentBranch')
-// const searchBranches = require('@gitmars/core/lib/git/searchBranches')
-
-// const error503 = (res: Response) => {
-// 	res.status(503).send({ data: null, success: false, code: 0, msg: 'fail' })
-// }
-// const success = (res: Response, { data, msg = 'success' }: any) => {
-//     res.status(200).send({ data, success: true, code: 1, msg })
-// }
 
 // 开启跨域访问
 router.all('*', (req: Request, res: Response, next: NextFunction) => {
@@ -36,10 +24,6 @@ router.all('*', (req: Request, res: Response, next: NextFunction) => {
     next()
 })
 
-// router.get('/cd', function (req: Request, res: Response) {
-//     process.chdir(decodeURIComponent(req.query.dir as string))
-//     success(res, { data: true })
-// })
 router.get('/cd', enterDir)
 
 // router.get('/result', function (req: Request, res: Response) {
@@ -48,37 +32,12 @@ router.get('/cd', enterDir)
 // })
 
 // 获取当前状态
-// router.get('/status', function (req: Request, res: Response) {
-//     res.status(200).send({ data: glob, success: true, code: 1, msg: 'success' })
-// })
 router.get('/status', getStatus)
-
 // 获取项目列表
-// router.get('/branch/list', (req: Request, res: Response) => {
-//     const { path, key, type, remote } = req.query
-//     // process.chdir('/Users/saqqdy/www/wojiayun/wyweb/webapp/app')
-//     const data = searchBranches({ path, key, type, remote })
-//     success(res, { data })
-// })
 router.get('/branch/list', getBranchList)
-
 // 获取项目列表
-// router.get('/branch/current', (req: Request, res: Response) => {
-//     const data = getCurrentBranch()
-//     success(res, { data })
-// })
 router.get('/branch/current', getCurrent)
-
 // 读取文件
-// router.get('/fs/read', (req: Request, res: Response) => {
-//     const {
-//         query: { path: dir }
-//     } = req
-//     const type = (dir as string).replace(/[\s\S]*\.([a-z]+)$/, '$1')
-//     let data = fs.readFileSync(dir).toString()
-//     if (type === 'json') data = JSON.parse(data)
-//     success(res, { data })
-// })
 router.get('/fs/read', readFile)
 
 module.exports = router
