9e67928ad9ce12b7aa783e7cfe56ff6f24859ef3
saqqdy <saqqdy@qq.com>
2022-06-07 22:31:37 +0800

    docs

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -1,9 +1,306 @@
 import { defineConfigWithTheme } from 'vitepress'
+import anchor from 'markdown-it-anchor'
 const isProduction = process.env.NODE_ENV === 'production'
+
+const sidebar = {
+    '/guide/': [
+        {
+            text: '指南',
+            collapsable: false,
+            items: [
+                {
+                    text: '介绍',
+                    link: '/guide/',
+                    collapsable: false
+                },
+                {
+                    text: '快速上手',
+                    link: '/guide/getting-start',
+                    collapsable: false
+                },
+                {
+                    text: '基本配置',
+                    link: '/guide/basic-config',
+                    collapsable: false
+                }
+            ]
+        }
+        // {
+        //     text: '深入',
+        //     collapsable: false,
+        //     items: [
+        //         {
+        //             text: '钩子',
+        //             link: 'hooks'
+        //         }
+        //     ]
+        // }
+    ],
+    '/example/': [
+        {
+            text: '工作流',
+            collapsable: false,
+            items: [
+                {
+                    text: 'feature',
+                    link: '/example/#feature'
+                },
+                {
+                    text: 'bugfix',
+                    link: '/example/#bugfix'
+                },
+                {
+                    text: 'support',
+                    link: '/example/#support'
+                },
+                {
+                    text: 'publish',
+                    link: '/example/#publish'
+                }
+            ]
+        },
+        {
+            text: '效率',
+            collapsable: false,
+            items: [
+                {
+                    text: 'merge',
+                    link: '/example/#merge'
+                },
+                {
+                    text: 'copy',
+                    link: '/example/#copy'
+                },
+                {
+                    text: 'build',
+                    link: '/example/#build'
+                },
+                {
+                    text: 'branch',
+                    link: '/example/#branch'
+                }
+            ]
+        }
+    ],
+    '/api/': [
+        {
+            text: '智能导航',
+            collapsable: false,
+            items: [
+                {
+                    text: 'gitm go',
+                    link: '/api/#gitm-go'
+                }
+            ]
+        },
+        {
+            text: '配置',
+            collapsable: false,
+            items: [
+                {
+                    text: 'gitm init',
+                    link: '/api/#gitm-init'
+                },
+                {
+                    text: 'gitm config',
+                    link: '/api/#gitm-config'
+                }
+            ]
+        },
+        {
+            text: '工作流',
+            collapsable: false,
+            items: [
+                {
+                    text: 'gitm start',
+                    link: '/api/#gitm-start'
+                },
+                {
+                    text: 'gitm combine',
+                    link: '/api/#gitm-combine'
+                },
+                {
+                    text: 'gitm end',
+                    link: '/api/#gitm-end'
+                },
+                {
+                    text: 'gitm update',
+                    link: '/api/#gitm-update'
+                },
+                {
+                    text: 'gitm continue',
+                    link: '/api/#gitm-continue'
+                }
+            ]
+        },
+        {
+            text: '效率',
+            collapsable: false,
+            items: [
+                {
+                    text: 'gitm merge',
+                    link: '/api/#gitm-merge'
+                },
+                {
+                    text: 'gitm copy',
+                    link: '/api/#gitm-copy'
+                },
+                {
+                    text: 'gitm build',
+                    link: '/api/#gitm-build'
+                },
+                {
+                    text: 'gitm branch',
+                    link: '/api/#gitm-branch'
+                },
+                {
+                    text: 'gitm revert',
+                    link: '/api/#gitm-revert'
+                },
+                {
+                    text: 'gitm undo',
+                    link: '/api/#gitm-undo'
+                },
+                {
+                    text: 'gitm redo',
+                    link: '/api/#gitm-redo'
+                },
+                {
+                    text: 'gitm save',
+                    link: '/api/#gitm-save'
+                },
+                {
+                    text: 'gitm get',
+                    link: '/api/#gitm-get'
+                },
+                {
+                    text: 'gitm cleanbranch',
+                    link: '/api/#gitm-cleanbranch'
+                },
+                {
+                    text: 'gitm log',
+                    link: '/api/#gitm-log'
+                },
+                {
+                    text: 'gitm hook',
+                    link: '/api/#gitm-hook'
+                },
+                {
+                    text: 'gitm run',
+                    link: '/api/#gitm-run'
+                },
+                {
+                    text: 'gitm upgrade',
+                    link: '/api/#gitm-upgrade'
+                },
+                {
+                    text: 'gitm clean',
+                    link: '/api/#gitm-clean'
+                },
+                {
+                    text: 'gitm suggest',
+                    link: '/api/#gitm-suggest'
+                },
+                {
+                    text: 'gitm approve',
+                    link: '/api/#gitm-approve'
+                },
+                {
+                    text: 'gitm review',
+                    link: '/api/#gitm-review'
+                },
+                {
+                    text: 'gitm status',
+                    link: '/api/#gitm-status'
+                },
+                {
+                    text: 'gitm ui',
+                    link: '/api/#gitm-ui'
+                },
+                {
+                    text: 'gitm link',
+                    link: '/api/#gitm-link'
+                },
+                {
+                    text: 'gitm unlink',
+                    link: '/api/#gitm-unlink'
+                },
+                {
+                    text: 'gitm postmsg',
+                    link: '/api/#gitm-postmsg'
+                },
+                {
+                    text: 'gitm alias',
+                    link: '/api/#gitm-alias'
+                }
+            ]
+        },
+        {
+            text: '管理员',
+            collapsable: false,
+            items: [
+                {
+                    text: 'gitm admin create',
+                    link: '/api/#gitm-admin-create'
+                },
+                {
+                    text: 'gitm admin publish',
+                    link: '/api/#gitm-admin-publish'
+                },
+                {
+                    text: 'gitm admin update',
+                    link: '/api/#gitm-admin-update'
+                },
+                {
+                    text: 'gitm admin clean',
+                    link: '/api/#gitm-admin-clean'
+                }
+            ]
+        },
+        {
+            text: '其他',
+            collapsable: false,
+            items: [
+                {
+                    text: 'gitm permission',
+                    link: '/api/#gitm-permission'
+                },
+                {
+                    text: 'gitm version',
+                    link: '/api/#gitm-version'
+                }
+            ]
+        }
+    ]
+    // '/hook/': [
+    //     {
+    //         text: '开始使用',
+    //         collapsable: false,
+    //         children: [
+    //             {
+    //                 text: '安装',
+    //                 link: '/hook/'
+    //             },
+    //             {
+    //                 text: '快速上手',
+    //                 link: '/hook/getting-start'
+    //             },
+    //             {
+    //                 text: '基本配置',
+    //                 link: '/hook/basic-config'
+    //             },
+    //             {
+    //                 text: '兼容性',
+    //                 link: '/hook/compatibility'
+    //             }
+    //         ]
+    //     }
+    // ]
+}
 
 module.exports = defineConfigWithTheme({
     title: 'Gitmars',
-    description:'可视化UI界面',
+    description: '可视化UI界面',
     base: '/gitmars/',
     outDir: 'dist',
     // theme: '@vuepress/default',
@@ -53,85 +350,7 @@ module.exports = defineConfigWithTheme({
                 ]
             }
         ],
-        sidebar: {
-            '/guide/': [
-                {
-                    text: '指南',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    items: [
-                        {
-                            text: '介绍',
-                            link: '/guide/',
-                            collapsable: false
-                        },
-                        {
-                            text: '快速上手',
-                            link: '/guide/getting-start',
-                            collapsable: false
-                        },
-                        {
-                            text: '基本配置',
-                            link: '/guide/basic-config',
-                            collapsable: false
-                        }
-                    ]
-                }
-                // {
-                //     text: '深入',
-                //     collapsable: false,
-                //     sidebarDepth: 1,
-                //     items: [
-                //         {
-                //             text: '钩子',
-                //             link: 'hooks',
-                //             collapsable: false
-                //         }
-                //     ]
-                // }
-            ],
-            // '/hook/': [
-            //     {
-            //         text: '开始使用',
-            //         collapsable: false,
-            //         // sidebarDepth: 1,
-            //         children: [
-            //             {
-            //                 text: '安装',
-            //                 link: '/hook/',
-            //                 collapsable: false
-            //             },
-            //             {
-            //                 text: '快速上手',
-            //                 link: '/hook/getting-start',
-            //                 collapsable: false
-            //             },
-            //             {
-            //                 text: '基本配置',
-            //                 link: '/hook/basic-config',
-            //                 collapsable: false
-            //             },
-            //             {
-            //                 text: '兼容性',
-            //                 link: '/hook/compatibility',
-            //                 collapsable: false
-            //             }
-            //         ]
-            //     },
-            //     {
-            //         text: '其他',
-            //         collapsable: false,
-            //         // sidebarDepth: 1,
-            //         children: [
-            //             {
-            //                 text: '特性',
-            //                 link: 'features',
-            //                 collapsable: false
-            //             }
-            //         ]
-            //     }
-            // ]
-        },
+        sidebar,
         lastUpdated: '最近更新',
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
         repo: 'https://github.com/saqqdy/gitmars.git',
@@ -149,7 +368,7 @@ module.exports = defineConfigWithTheme({
         editLinks: true,
         // 默认为 "Edit this page"
         editLinkText: '帮助我们改善此页面！'
-    }
+    },
     // plugins: [
     //     // 官方回到顶部插件
     //     '@vuepress/back-to-top',
@@ -173,11 +392,16 @@ module.exports = defineConfigWithTheme({
     //         }
     //     ]
     // ],
-    // markdown: {
-    //     lineNumbers: false,
-    //     toc: { includeLevel: [2, 3] }
-    // },
-    // sidebarDepth: 1,
+    markdown: {
+        lineNumbers: false,
+        // anchor: {
+        //     permalink: anchor.permalink.headerLink()
+        // },
+        toc: { level: [2, 3] },
+        config: md => {
+            //   md.use(require('markdown-it-xxx'))
+        }
+    }
     // configureWebpack: {
     //     resolve: {
     //         alias: {
