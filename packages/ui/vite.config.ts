2e2429c356ba56c1ab215800e3f3618c47b61fbe
saqqdy <saqqdy@qq.com>
2022-09-12 01:33:02 +0800

    release 4.0.0-alpha.11

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,5 +1,5 @@
 import { dirname, resolve, join } from 'path'
-// import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'url'
 import { existsSync } from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,8 +10,8 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
