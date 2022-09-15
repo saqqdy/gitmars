b7d1f96dba5b6c1fa4726149f4b414a96814bd67
saqqdy <saqqdy@qq.com>
2022-09-16 01:58:26 +0800

    use import

--- build/packages.ts
+++ build/packages.ts
@@ -1,28 +1,28 @@
 import type { PackageManifest } from './types'
 
 export const packages: PackageManifest[] = [
-    // {
-    //     name: 'core',
-    //     pkgName: '@gitmars/core',
-    //     buildTask: 'lib',
-    //     iife: false,
-    //     cjs: true,
-    //     mjs: true,
-    //     dts: true,
-    //     output: 'lib',
-    //     display: 'gitmars核心程序'
-    // },
-    // {
-    //     name: 'gitmars',
-    //     pkgName: 'gitmars',
-    //     buildTask: 'lib',
-    //     iife: false,
-    //     cjs: false,
-    //     mjs: true,
-    //     dts: false,
-    //     output: 'lib',
-    //     display: '这是一个git工作流工具'
-    // },
+    {
+        name: 'core',
+        pkgName: '@gitmars/core',
+        buildTask: 'lib',
+        iife: false,
+        cjs: true,
+        mjs: true,
+        dts: true,
+        output: 'lib',
+        display: 'gitmars核心程序'
+    },
+    {
+        name: 'gitmars',
+        pkgName: 'gitmars',
+        buildTask: 'lib',
+        iife: false,
+        cjs: false,
+        mjs: true,
+        dts: false,
+        output: 'lib',
+        display: '这是一个git工作流工具'
+    },
     {
         name: 'server',
         pkgName: '@gitmars/server',
