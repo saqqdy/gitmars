b5945bb7f4fda19e81223dcb93c75393885055ad
saqqdy <saqqdy@qq.com>
2022-09-18 19:11:45 +0800

    fix top-level await

--- packages/core/src/locales/index.ts
+++ packages/core/src/locales/index.ts
@@ -1,12 +1,12 @@
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import { I18n } from 'i18n'
-import { osLocale } from 'os-locale'
+import { osLocaleSync } from 'os-locale'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const locales = ['en-US', 'zh-CN']
-const locale = process.env.GITMARS_LANG || (await osLocale())
+const locale = process.env.GITMARS_LANG || osLocaleSync()
 
 const i18n = new I18n({
     locales,
