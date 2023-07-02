547c62b932ca62beb0255302b51aa78d29588571
saqqdy <saqqdy@qq.com>
2023-07-02 19:12:57 +0800

    hide no-need logs

--- packages/core/src/lang.ts
+++ packages/core/src/lang.ts
@@ -1,11 +1,11 @@
-import osLocale from 'os-locale'
+import { osLangSync } from 'os-lang'
 import { useLocale } from '#lib/utils/local'
 import * as languages from '#lib/locales/index'
 
 export type LanguageType = Exclude<keyof typeof languages, 'default'>
 
 const locales: LanguageType[] = ['enUS', 'zhCN']
-let locale = (process.env.GITMARS_LANG || osLocale.sync()).replace('-', '') as LanguageType
+let locale = (process.env.GITMARS_LANG || osLangSync()).replace('-', '') as LanguageType
 if (!locales.includes(locale)) locale = 'enUS'
 
 export default useLocale(languages[locale])
